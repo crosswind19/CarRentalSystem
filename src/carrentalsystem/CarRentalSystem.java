32d076235c9d68c4e65e4347e93fb6f662f9a721
BBen123 <benlimguodong@gmail.com>
2022-10-01 13:20:42 +0800

    Testing again

--- src/carrentalsystem/CarRentalSystem.java
+++ src/carrentalsystem/CarRentalSystem.java
@@ -17,7 +17,7 @@ public class CarRentalSystem {
         // TODO code application logic here
         
         //Test Test
-        System.out.println("Testing from Ben");
+        System.out.println("Testing from Ben123");
     }
     
 }
