83467e6b1d98f41acbc8dcf29335eac11837ab48
crosswind19 <70271667+crosswind19@users.noreply.github.com>
2022-12-04 22:37:37 +0800

    Customer aggregiation

---
+++ src/Class/Address.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+
+/**
+ *
+ * @author Crosswind Cheah
+ */
+public class Address {
+    String Street;
+    String Postal;
+    String City;
+    String State;
+    
+    public Address(String Street,String Postal, String City,String State){
+        this.Street = Street;
+        this.Postal = Postal;
+        this.City = City;
+        this.State = State;
+    }
+}
